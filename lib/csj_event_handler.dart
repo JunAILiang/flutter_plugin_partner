0af2b4ddf202a46f891c003b5646eac7849afd98
刘俊敏 <gzliujm@163.com>
2019-12-10 16:31:05 +0800

    android ad

---
+++ lib/csj_event_handler.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/services.dart';
+
+
+abstract class CSJEventHandler {
+  final Function(CSJEvents, Map<String, dynamic>) _listener;
+
+  CSJEventHandler(Function(CSJEvents, Map<String, dynamic>) listener) : _listener = listener;
+
+  Future<dynamic> handleEvent(MethodCall call) async {
+    switch (call.method) {
+      case 'didSuccess':
+        _listener(CSJEvents.didSuccess, null);
+        break;
+      case 'didFail':
+        _listener(CSJEvents.didFail, null);
+        break;
+      case 'loadSuccess':
+        _listener(CSJEvents.loadSuccess, null);
+        break;
+      case 'playStart':
+        _listener(CSJEvents.playStart, null);
+        break;
+      case 'playEnd':
+        _listener(CSJEvents.playEnd, null);
+        break;
+      case 'clicked':
+        _listener(CSJEvents.clicked, null);
+        break;
+      case 'didError':
+        _listener(CSJEvents.didError, null);
+        break;
+      case 'playError':
+        _listener(CSJEvents.playError, null);
+        break;
+      case 'onAdClose':
+        _listener(CSJEvents.onAdClose, null);
+        break;
+    }
+
+    return null;
+  }
+
+}
+
+enum CSJEvents {
+  didSuccess,  // 有广告返回
+  didFail, // 无广告返回
+  loadSuccess, // 广告加载成功
+  playStart, // 广告开始播放
+  playEnd, // 广告播放完毕
+  clicked, // 广告发生点击
+  didError, // 广告发生错误
+  playError, // 播放时发生错误
+  onAdClose, // 广告播放完毕并点击了关闭按钮
+}
